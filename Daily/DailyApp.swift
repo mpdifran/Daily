e8cc65f5c27b07db3ab989dd76b44328987251d4
Mark DiFranco <mpdifranco@icloud.com>
2023-04-02 11:26:47 -0400

    Initial Commit

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by Mark DiFranco on 2023-04-02.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
